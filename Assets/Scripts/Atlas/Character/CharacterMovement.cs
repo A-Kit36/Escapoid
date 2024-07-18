50cbc34996e793c93895aa4798fdec41a78973d6
Stephen <168591694+PiccoloArkouda@users.noreply.github.com>
2024-07-18 14:14:43 -0400

    Movement Work
    Input buffer for turning without moving.
    Handling collisions with 'obstacles' now.

--- Assets/Scripts/Atlas/Character/CharacterMovement.cs
+++ Assets/Scripts/Atlas/Character/CharacterMovement.cs
@@ -8,8 +8,13 @@ public class CharacterMovement : CharacterAbility
 {
     [SerializeField]
     private float _moveSpeed = 1f;
+    [SerializeField]
+    private float _turnTime = 0.2f;
+
     private bool _isMoving = false;
     private Cardinal _commandDirection;
+    private Coroutine _movementCoroutine;
+
     protected override void AbilityStart()
     {        
         if (Character.FacingDirection != _commandDirection)
@@ -18,7 +23,6 @@ public class CharacterMovement : CharacterAbility
         }
         else
         {
-            //move character
             MoveCharacter(_commandDirection);         
         }  
     }
@@ -28,53 +32,55 @@ public class CharacterMovement : CharacterAbility
     }
     protected override void ProcessAbility()
     {
-        if (InputManager.Instance.Horizontal > 0f)
+        if (!_isMoving)
         {
-            //If facing East, move East else face East.
-            _commandDirection = Cardinal.East;
-            if (IsAbilityPermitted)
+            if (InputManager.Instance.Horizontal > 0f)
             {
-                AbilityStart();
+                //If facing East, move East else face East.
+                _commandDirection = Cardinal.East;
+                if (IsAbilityPermitted)
+                {
+                    AbilityStart();
+                }
             }
-        }
-        else if(InputManager.Instance.Horizontal < 0f)
-        {
-            //If facing West, move West else face West.
-            _commandDirection = Cardinal.West;
-            if (IsAbilityPermitted)
+            else if (InputManager.Instance.Horizontal < 0f)
             {
-                AbilityStart();
+                //If facing West, move West else face West.
+                _commandDirection = Cardinal.West;
+                if (IsAbilityPermitted)
+                {
+                    AbilityStart();
+                }
             }
-        }
-        else if (InputManager.Instance.Vertical > 0f)
-        {
-            //If facing North, move North else face North.
-            _commandDirection = Cardinal.North;
-            if (IsAbilityPermitted)
+            else if (InputManager.Instance.Vertical > 0f)
             {
-                AbilityStart();
+                //If facing North, move North else face North.
+                _commandDirection = Cardinal.North;
+                if (IsAbilityPermitted)
+                {
+                    AbilityStart();
+                }
             }
-        }
-        else if (InputManager.Instance.Vertical < 0f)
-        {
-            //If facing South, move South else face South.
-            _commandDirection = Cardinal.South;
-            if (IsAbilityPermitted)
+            else if (InputManager.Instance.Vertical < 0f)
             {
-                AbilityStart();
+                //If facing South, move South else face South.
+                _commandDirection = Cardinal.South;
+                if (IsAbilityPermitted)
+                {
+                    AbilityStart();
+                }
             }
-        }
-        else
-        {
-            //AbilityEnd();
-        }
+        }      
     }
     private void MoveCharacter(Cardinal commandDirection)
     {
         Vector3 targetPos = transform.position + Character.CardinalToVect3(commandDirection);
-        if (!_isMoving)
+        Vector2 rayDirection = Character.CardinalToVect3(commandDirection);
+        //Debug.DrawRay(transform.position, rayDirection, Color.red, 2f);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, rayDirection, 1f, LayerMask.GetMask("Obstacle"));
+        if (!_isMoving && hit.collider == null)
         {
-            StartCoroutine(MoveToPosition(targetPos));
+            _movementCoroutine = StartCoroutine(MoveToPosition(targetPos));
         }
     }
 
@@ -95,14 +101,26 @@ public class CharacterMovement : CharacterAbility
         Character.FacingDirection = dir;
         Vector3 faceDir = Character.GetDirectionScale(dir);
 
+        StartCoroutine(TurnCharacter(faceDir));        
+
+    }
+
+    private IEnumerator TurnCharacter(Vector3 faceDir)
+    {
+        _isMoving = true;
+
         if (Character.CharacterModel != null)
-        {            
+        {
             Character.CharacterModel.transform.localScale = faceDir;
         }
         else
-        {            
-            transform.localScale = faceDir;            
+        {
+            transform.localScale = faceDir;
         }
+        
+        yield return new WaitForSeconds(_turnTime);
+        
+        _isMoving = false;
 
     }
 
